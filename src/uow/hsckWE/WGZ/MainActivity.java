2da94cd6f79b18c06df50268ca878cb85fda60ba
Adrien <adriengagnon07@gmail.com>
2013-01-19 17:33:11 -0500

    Set Changes to the String class and GPS System

--- src/uow/hsckWE/WGZ/MainActivity.java
+++ src/uow/hsckWE/WGZ/MainActivity.java
@@ -1,22 +1,71 @@
 package uow.hsckWE.WGZ;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.EditText;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements LocationListener {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    private LocationManager lManager;
+    private EditText lat;
+    private EditText lon;
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lat = (EditText)findViewById(R.id.editText1);
+        lon = (EditText)findViewById(R.id.editText2);
+        
+        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000L, 500.0f, this);
 
+
+        lManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (lManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+            lManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                    0, 0, this);
+        else
+            Log.i("Test", "network provider unavailable");
+
+        Location lastKnownLocation =
+                lManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+        if (lastKnownLocation != null) {
+            Log.i("Test", lastKnownLocation.getLatitude() + ", "
+                    + lastKnownLocation.getLongitude());
+        } else
+            Log.i("Test", "null");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    public void onLocationChanged(Location location) {
+    	if (location != null) {
+    		double lat = location.getLatitude();
+    		double lng = location.getLongitude();
+ //   		p = new GeoPoint((int) lat * 1000000, (int) lng * 1000000);
+ //   		mc.animateTo(p);
+    	}
+
+    }
+
+    public void onProviderDisabled(String provider) {
+    }
+
+    public void onProviderEnabled(String provider) {
+    }
+
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
 }
---
+++ src/uow/hsckWE/WGZ/NewWindow.java
@@ -0,0 +1,11 @@
+package uow.hsckWE.WGZ;
+
+import android.app.Activity;
+import android.os.Bundle;
+public class NewWindow extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
